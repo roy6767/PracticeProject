00fb9c3552079d416a760059ff2740d88a921e0e
roy6767 <roy.biplobslu@gmail.com>
2025-05-05 03:04:34 +0200

    Added Google As Oauth2

--- src/main/java/org/example/practiceproject1/controllers/HomeController.java
+++ src/main/java/org/example/practiceproject1/controllers/HomeController.java
@@ -2,6 +2,7 @@ package org.example.practiceproject1.controllers;
 
 import org.example.practiceproject1.models.HockeyPlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,5 +27,10 @@ public class HomeController extends BaseController{
         String user =getLoggedInEmail();
         model.addAttribute("user",user);
         return "profile";
+    }
+    @GetMapping("/logout")
+    public String logout() {
+        SecurityContextHolder.clearContext();
+        return "redirect:/";
     }
 }
